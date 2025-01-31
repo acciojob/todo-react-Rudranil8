fa5edad718d48e234e7f7aadacc1993dd35bb797
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2025-01-31 15:03:06 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -134,41 +134,37 @@
 
 // export default App
 
-
-
 import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
-  let [todos,setTodos]=useState([])
-  let [newTodo,setNewTodo]=useState('')
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
 
-  function addTodo(){
-    if(newTodo.trim()!==''){
-      setTodos([...todos,newTodo]);
+  const addTodo = () => {
+    const trimmedTodo = newTodo.trim();
+    if(trimmedTodo !== ''){
+      setTodos([...todos, trimmedTodo]);
       setNewTodo('');
     }
   }
 
-  function removeTodo(i){
-    let updatedTodos=todos.filter((element,index)=>{
-      return i!==index;
-    })
-    setTodos(updatedTodos)
+  const removeTodo = (index) => {
+    const updatedTodos = todos.filter((element, i) => i !== index);
+    setTodos(updatedTodos);
   }
 
   return (
     <div className="main">
-        {/* Do not remove the main div */}
         <h1>To-D0 List</h1>
-        <input type="text" value={newTodo} onChange={(e)=>setNewTodo(e.target.value)}  />
+        <input type="text" value={newTodo} onChange={(e) => setNewTodo(e.target.value)}  />
         <button onClick={addTodo}>Add Todo</button>
         <ul>
         {
-          todos.map((element,index)=>(
+          todos.map((element, index) => (
             <li key={index}>
               <div>{element}</div>
-              <button onClick={()=>removeTodo(index)}>Delete</button>
+              <button onClick={() => removeTodo(index)}>Delete</button>
             </li>
           ))
         }
