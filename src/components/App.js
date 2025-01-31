1d5e911fe353087d368122b2c22cdc5e1c386f6d
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2025-01-31 12:10:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -87,6 +87,55 @@
 // }
 
 // export default App
+
+
+
+
+// import React, { useState } from "react";
+// import './../styles/App.css';
+
+// const App = () => {
+//   let [todos,setTodos]=useState([])
+//   let [newTodo,setNewTodo]=useState('')
+
+//   function addTodo(){
+//     if(newTodo.trim()!==''){
+//       setTodos([...todos,newTodo]);
+//       setNewTodo('');
+//     }
+//   }
+
+//   function removeTodo(i){
+//     let updatedTodos=todos.filter((element,index)=>{
+//       return i!==index;
+//     })
+//     setTodos(updatedTodos)
+//   }
+
+//   return (
+//     <div className="main">
+//         {/* Do not remove the main div */}
+//         <h1>To-D0 List</h1>
+//         <input type="text" value={newTodo} onChange={(e)=>setNewTodo(e.target.value)}  />
+//         <button onClick={addTodo}>Add Todo</button>
+//         <ul>
+//         {
+//           todos.map((element,index)=>(
+//             <li key={index}>
+//               <div>{element}</div>
+//               <button onClick={()=>removeTodo(index)}></button>
+//             </li>
+//           ))
+//         }
+//         </ul>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
 import React, { useState } from "react";
 import './../styles/App.css';
 
@@ -119,7 +168,7 @@ const App = () => {
           todos.map((element,index)=>(
             <li key={index}>
               <div>{element}</div>
-              <button onClick={()=>removeTodo(index)}></button>
+              <button onClick={()=>removeTodo(index)}>Delete</button>
             </li>
           ))
         }
@@ -128,4 +177,4 @@ const App = () => {
   )
 }
 
-export default App
+export default App;
